45a3662201cb7623aa4907ba9556b4d08cb889c2
Ando <iamando@tuta.io>
2024-04-22 20:25:27 +0300

    feat: impl gcp cloud providers and setup terraform, variables, providers

---
+++ gcp/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type = string
+  description = "The id of the google cloud project."
+}
+
+variable "region" {
+  type = string
+  description = "The region where is located the vm."
+}
