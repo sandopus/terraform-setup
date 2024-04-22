3faef36044d94275b6f206528642778da5b7b0e5
Ando <iamando@tuta.io>
2024-04-22 20:46:08 +0300

    feat: configure gsc as backend and setup bucket, prefix variables

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -7,3 +7,13 @@ variable "region" {
   type = string
   description = "The region where is located the vm."
 }
+
+variable "bucket" {
+  type = string
+  description = "The bucket name in the google cloud storage."
+}
+
+variable "prefix" {
+  type = string
+  description = "The prefix name of the bucket."
+}
