e3b1223b3faeb5ad313b51f6dc53b8638e190ed2
Ando <iamando@tuta.io>
2024-04-23 13:48:35 +0300

    feat: use text as uppercase for variable and add google provider version to it (#5)

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -1,19 +1,26 @@
-variable "project_id" {
+variable "GOOGLE_PROJECT_ID" {
   type = string
   description = "The id of the google cloud project."
 }
 
-variable "region" {
+variable "GOOGLE_PROJECT_REGION" {
   type = string
   description = "The region where is located the vm."
 }
 
-variable "bucket" {
+variable "GOOGLE_PROJECT_BUCKET" {
   type = string
   description = "The bucket name in the google cloud storage."
 }
 
-variable "prefix" {
+variable "GOOGLE_PROJECT_BUCKET_PREFIX" {
   type = string
   description = "The prefix name of the bucket."
+}
+
+variable "GOOGLE_PROVIDER_VERSION" {
+  type = string
+  description = "The version of hashicorp google provider."
+
+  default = "5.25.0"
 }
