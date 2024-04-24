74058db9641eb4df4aecd32aa304ea4a45a7650c
Ando <iamando@tuta.io>
2024-04-24 17:15:06 +0300

    chore: use recommanded typo (#9)
    * feat: use text as uppercase for variable and add google provider version to it
    
    * feat: setup infisical andn provide secret from env
    
    * feat: setup gcs as backend provider for gcp and pull secrets from infisical
    
    * feat: create gh workflows for automate creating pull request
    
    * chore: use recommanded typo

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,7 +1,7 @@
 output "GOOGLE_PROJECT_ID" {
-  value = data.infisical_secrets.terraform-setup-secrets.secrets["GOOGLE_PROJECT_ID"]
+  value = data.infisical_secrets.terraform_setup_secrets.secrets["GOOGLE_PROJECT_ID"]
 }
 
 output "GOOGLE_PROJECT_BUCKET" {
-  value = data.infisical_secrets.terraform-setup-secrets.secrets["GOOGLE_PROJECT_BUCKET"]
+  value = data.infisical_secrets.terraform_setup_secrets.secrets["GOOGLE_PROJECT_BUCKET"]
 }
